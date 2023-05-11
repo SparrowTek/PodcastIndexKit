9ea25fed2605e6cd3475f9bce19a61f70b71805c
Thomas Rademaker <thomas@sparrowtek.com>
2023-05-11 12:01:09 -0400

    complete podcastsService

--- Sources/PodcastIndexKit/Services/PodcastsService.swift
+++ Sources/PodcastIndexKit/Services/PodcastsService.swift
@@ -5,6 +5,7 @@
 //  Created by Thomas Rademaker on 5/8/23.
 //
 
+import Foundation
 import Get
 
 public struct PodcastsService {
@@ -67,12 +68,89 @@ public struct PodcastsService {
     ///- parameter pretty: If present, makes the output “pretty” to help with debugging.
     ///Parameter shall not have a value
     ///- returns: a  `SearchResult` object which has an array of `Podcast`s
-//    public func podcastByTag(max: Int? = nil, startAt: String? = nil, pretty: Bool? = nil) async throws -> PodcastArrayResult {
-//        
-//    }
+    public func podcastByTag(max: Int? = nil, startAt: String? = nil, pretty: Bool? = nil) async throws -> PodcastArrayResult {
+        try await podcasts(path: "bytag", q: ("podcast-value", nil), max: max, pretty: pretty, startAt: startAt)
+    }
     
-    /// Helper method to facilitate all podcasts methods
-    ///
+    /// This call returns all feeds marked with the specified [medium](https://github.com/Podcastindex-org/podcast-namespace/blob/main/docs/1.0.md#medium) tag value.
+    ///- parameter medium: (Required) The medium value to search for.
+    /// Full list of possible values documented in [medium](https://github.com/Podcastindex-org/podcast-namespace/blob/main/docs/1.0.md#medium) tag spec.
+    ///- parameter max: Maximum number of results to return.
+    ///- parameter start_at: Feed ID to start at for request
+    ///- parameter pretty: If present, makes the output “pretty” to help with debugging.
+    ///Parameter shall not have a value
+    ///- returns: a  `SearchResult` object which has an array of `Podcast`s
+    public func podcast(byMedium medium: String, max: Int? = nil, pretty: Bool? = nil) async throws -> PodcastArrayResult {
+        try await podcasts(path: "bymedium", q: ("medium", medium), max: max, pretty: pretty)
+    }
+    
+    /// This call returns the podcasts/feeds that in the index that are trending.
+    /// - parameter max: Maximum number of results to return.
+    /// - parameter since: Return items since the specified time. The value can be a unix epoch timestamp or a negative integer that represents a number of seconds prior to right now.
+    /// - parameter lang: Specifying a language code (like "en") will return only episodes having that specific language.
+    /// You can specify multiple languages by separating them with commas.
+    /// If you also want to return episodes that have no language given, use the token "unknown". (ex. en,es,ja,unknown).
+    /// Values are not case sensitive.
+    /// - parameter cat: Use this argument to specify that you ONLY want episodes with these categories in the results.
+    /// Separate multiple categories with commas.
+    /// You may specify either the Category ID and/or the Category Name.
+    /// Values are not case sensitive.
+    /// The cat and notcat filters can be used together to fine tune a very specific result set.
+    /// Category numbers and names can be found in the [Podcast Namespace documentation](https://github.com/Podcastindex-org/podcast-namespace/blob/main/categories.json)
+    /// - parameter notcat: Use this argument to specify categories of episodes to NOT show in the results.
+    /// Separate multiple categories with commas.
+    /// You may specify either the Category ID and/or the Category Name.
+    /// Values are not case sensitive.
+    /// The cat and notcat filters can be used together to fine tune a very specific result set.
+    /// Category numbers and names can be found in the [Podcast Namespace documentation](https://github.com/Podcastindex-org/podcast-namespace/blob/main/categories.json)
+    ///- parameter pretty: If present, makes the output “pretty” to help with debugging.
+    ///- returns: a  `SearchResult` object which has an array of `Podcast`s
+    public func trendingPodcasts(max: Int? = nil, since: Date? = nil, lang: String? = nil, cat: String? = nil, notcat: String? = nil, pretty: Bool? = nil) async throws -> PodcastArrayResult {
+        var query: [(String, String?)]? = []
+                
+        if let max {
+            query?.append(("max", "\(max)"))
+        }
+        
+        if let pretty, pretty == true {
+            query?.append(("pretty", nil))
+        }
+        
+        if let since {
+            query?.append(("since", "\(since)"))
+        }
+        
+        if let lang {
+            query?.append(("lang", lang))
+        }
+        
+        if let cat {
+            query?.append(("cat", cat))
+        }
+        
+        if let notcat {
+            query?.append(("notcat", notcat))
+        }
+        
+        return try await apiClient.send(Request(path: "\(basePath)/trending", query: query)).value
+    }
+    
+    /// This call returns all feeds that have been marked dead (dead == 1)
+    /// Dead feeds can also be accessed from object storage at [https://public.podcastindex.org/podcastindex_dead_feeds.csv](https://public.podcastindex.org/podcastindex_dead_feeds.csv)
+    /// - parameter pretty: If present, makes the output “pretty” to help with debugging.
+    /// Parameter shall not have a value
+    ///- returns: a  `SearchResult` object which has an array of `Podcast`s
+    public func deadPodcasts(pretty: Bool? = nil) async throws -> PodcastArrayResult {
+        var query: [(String, String?)]?
+        
+        if let pretty, pretty == true {
+            query = [("pretty", nil)]
+        }
+        
+        return try await apiClient.send(Request(path: "\(basePath)/dead", query: query)).value
+    }
+    
+    /// Helper method to facilitate all podcast methods that return a `PodcastResult`
     /// - parameter path: the path to append to the base path
     /// - parameter q: (Required) query to search for
     /// - parameter pretty: If present, makes the output “pretty” to help with debugging. Parameter shall not have a value
@@ -82,6 +160,29 @@ public struct PodcastsService {
         
         if let pretty, pretty == true {
             query?.append(("pretty", nil))
+        }
+        
+        return try await apiClient.send(Request(path: "\(basePath)/\(path)", query: query)).value
+    }
+    
+    /// Helper method to facilitate all podcast methods that return a `PodcastArrayResult`
+    /// - parameter path: the path to append to the base path
+    /// - parameter q: (Required) query to search for
+    /// - parameter pretty: If present, makes the output “pretty” to help with debugging. Parameter shall not have a value
+    /// - returns: a `PodcastArrayResult` object which is has an embedded `Podcast` array.
+    private func podcasts(path: String, q: (String, String?), max: Int? = nil, pretty: Bool? = nil, startAt: String? = nil) async throws -> PodcastArrayResult {
+        var query: [(String, String?)]? = [q]
+                
+        if let max {
+            query?.append(("max", "\(max)"))
+        }
+        
+        if let pretty, pretty == true {
+            query?.append(("pretty", nil))
+        }
+        
+        if let startAt {
+            query?.append(("start_at", startAt))
         }
         
         return try await apiClient.send(Request(path: "\(basePath)/\(path)", query: query)).value
