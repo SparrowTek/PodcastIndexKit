d1cb37cf6f8920cfd1ee0c88c45b1b699a567e96
Thomas Rademaker <thomas@sparrowtek.com>
2023-10-25 09:17:01 -0400

    port AppleReplacementService to in house networking code

--- Sources/PodcastIndexKit/Networking.swift
+++ Sources/PodcastIndexKit/Networking.swift
@@ -43,6 +43,8 @@ var appleReplacementConfiguration: APIClient.Configuration = {
     return config
 }()
 
+var appleReplacementBaseURL = "https://api.podcastindex.org"
+
 class PodcastIndexAPIClientDelegate: APIClientDelegate {
     func client(_ client: APIClient, willSendRequest request: inout URLRequest) async throws {
         let errorMessage = """
@@ -78,5 +80,32 @@ Hint: You must call the static setup(apiKey: String, apiSecret: String, userAgen
 
     func client<T>(_ client: APIClient, makeURLForRequest request: Request<T>) throws -> URL? {
         nil // Use default handlings
+    }
+}
+
+
+class PodcastIndexRouterDelegate: NetworkRouterDelegate {
+    func intercept(_ request: inout URLRequest) async {
+        let errorMessage = """
+PODCASTINDEXKIT Error: your apiKey, secretKey, and userAgent were not set.
+Please follow the intructions in the README for setting up the PodcastIndexKit framework
+Hint: You must call the static setup(apiKey: String, apiSecret: String, userAgent: String) method before using the framework
+"""
+        guard let apiKey = PodcastIndexKit.apiKey, let apiSecret = PodcastIndexKit.apiSecret, let userAgent = PodcastIndexKit.userAgent else { fatalError(errorMessage) }
+        
+        // prep for crypto
+        let apiHeaderTime = String(Int(Date().timeIntervalSince1970))
+        let data4Hash = apiKey + apiSecret + "\(apiHeaderTime)"
+        
+        // ======== Hash them to get the Authorization token ========
+        let inputData = Data(data4Hash.utf8)
+        let hashed = Insecure.SHA1.hash(data: inputData)
+        let hashString = hashed.compactMap { String(format: "%02x", $0) }.joined()
+        
+        // set Headers
+        request.addValue(apiHeaderTime, forHTTPHeaderField: "X-Auth-Date")
+        request.addValue(apiKey, forHTTPHeaderField: "X-Auth-Key")
+        request.addValue(hashString, forHTTPHeaderField: "Authorization")
+        request.addValue(userAgent, forHTTPHeaderField: "User-Agent")
     }
 }
