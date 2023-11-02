7ab8dc0a6192ab1e3303c9de1d2075232cae3e8a
Thomas Rademaker <thomas@sparrowtek.com>
2023-11-01 21:30:44 -0400

    remove unused dependencies

--- Package.swift
+++ Package.swift
@@ -17,16 +17,11 @@ let package = Package(
             targets: ["PodcastIndexKit"]),
     ],
     dependencies: [
-        .package(url: "https://github.com/kean/Get", .upToNextMajor(from: "2.1.6")),
-        .package(url: "https://github.com/apple/swift-crypto", .upToNextMajor(from: "2.5.0")),
         .package(url: "https://github.com/apple/swift-docc-plugin", from: "1.0.0"),
     ],
     targets: [
         .target(
             name: "PodcastIndexKit",
-            dependencies: [
-                "Get",
-                .product(name: "Crypto", package: "swift-crypto"),
-            ]),
+            dependencies: []),
     ]
 )
--- Sources/PodcastIndexKit/Networking.swift
+++ Sources/PodcastIndexKit/Networking.swift
@@ -1,5 +1,5 @@
 import Foundation
-import Crypto // TODO: can I remove crypto dependency ??
+import Crypto
 
 extension JSONDecoder {
     static var podcastIndexDecoder: JSONDecoder {        
