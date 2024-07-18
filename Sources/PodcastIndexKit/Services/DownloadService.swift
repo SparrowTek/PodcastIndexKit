2d800c448a9576c7c45f2a28710067066b9f01f3
Thomas Rademaker <thomas@radmakr.com>
2024-07-17 22:44:28 -0400

    make DownloadService public

--- Sources/PodcastIndexKit/Services/DownloadService.swift
+++ Sources/PodcastIndexKit/Services/DownloadService.swift
@@ -1,11 +1,11 @@
 @preconcurrency import Foundation
 
 @PodcastActor
-class DownloadService: Sendable {
+public class DownloadService: Sendable {
     public init() {}
     
-    public func downloadEpisode(from url: String) async throws -> Data {
-        guard let url = URL(string: url) else { throw NetworkError.missingURL }
+    public func downloadEpisode(from url: String?) async throws -> Data {
+        guard let url, let url = URL(string: url) else { throw NetworkError.missingURL }
         let request = URLRequest(url: url)
         guard let (data, response) = try? await URLSession.shared.data(for: request, delegate: nil) else { throw NetworkError.noData }
         guard let httpResponse = response as? HTTPURLResponse else { throw NetworkError.noStatusCode }
